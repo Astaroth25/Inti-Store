9df1931fea61f85adc08692f148581b8c99b294d
Astaroth_25 <angelo0984841543@gmail.com>
2025-04-21 10:05:34 -0500

    Componente contacto.

--- src/app/Features/contacts/contacts.component.ts
+++ src/app/Features/contacts/contacts.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
+import { ButtonComponent } from "../../Shared/button/button.component";
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [ReactiveFormsModule, ButtonComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
+  contactForm = new FormGroup({
+  name: new FormControl(''),
+  phone: new FormControl(''),
+  email: new FormControl(''),
+  message: new FormControl(''),
+});
 
+submit(){
+  console.log(this.contactForm.value);
+}
 }
